91ac4fc08e70daaa1e6ca3ce151270689e716359
gustavotassi <souzagustavotassi@gmail.com>
2020-09-23 15:57:28 -0300

    Project finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {
-
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), BrowserAnimationsModule],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
 
   title = 'angular-activity-form';
+  hide = true;
+  arrN: any[] = [];
 
   contactForm: FormGroup;
   constructor(private formBuilder: FormBuilder) {
     this.createContactForm();
   }
 
-  hide = true;
+  ngOnInit(): void {
+    const verificaLocal = JSON.parse(localStorage.getItem('people'));
+
+    if (verificaLocal !== null) {
+      this.arrN = verificaLocal;
+    }
+  }
 
   createContactForm() {
     this.contactForm = this.formBuilder.group({
@@ -39,24 +48,55 @@ export class AppComponent {
         [Validators.required,
         Validators.minLength(4)]
       ],
-      people: this.formBuilder.array([
-        this.formBuilder.control('')
-      ])
     });
   }
 
   onSubmit() {
     console.log(this.contactForm.value);
-    console.log(this.contactForm.status);
 
-    // Limpando os valores dos campos -- VERIFICAR //
+    // Preciso criar uma array dos valores do form
+    // Adicionando a cada submit os valores inseridos
+    // Criando uma var de array
+    let arr: any[] = [];
+    // Criando o verifica local para saber o que tem no meu localStorage
+    const verificaLocal = JSON.parse(localStorage.getItem('people'));
 
-    this.contactForm.patchValue({
-      name: '',
-      surname: '',
-      email: '',
-      cpf: '',
-      password: '',
-    });
+    console.log(verificaLocal);
+
+    if (verificaLocal !== null) {
+      arr = verificaLocal;
+      arr.push(this.contactForm.value);
+      localStorage.setItem('people', JSON.stringify(arr));
+      this.arrN.push(this.contactForm.value);
+    } else {
+      arr = [this.contactForm.value];
+      localStorage.setItem('people', JSON.stringify(arr));
+      this.arrN.push(this.contactForm.value);
+    }
+
+    // Limpando os valores dos campos sem erro //
+
+    this.contactForm.reset();
+  }
+
+  deleteEntry(element) {
+      const arrP: any[] = JSON.parse(localStorage.getItem('people'));
+      let indexDelete: number;
+      arrP.forEach((p, i) => {
+        if (p.name === element.name && p.cpf === element.cpf) {
+          indexDelete = i;
+        }
+      });
+      this.arrN.splice(indexDelete, 1);
+      arrP.splice(indexDelete, 1);
+      localStorage.setItem('people', JSON.stringify(arrP));
+  }
+
+  updateEntry(element) {
+    this.contactForm.get('name').setValue(element.name);
+    this.contactForm.get('surname').setValue(element.surname);
+    this.contactForm.get('email').setValue(element.email);
+    this.contactForm.get('cpf').setValue(element.cpf);
+    this.contactForm.get('password').setValue(element.password);
   }
 }
